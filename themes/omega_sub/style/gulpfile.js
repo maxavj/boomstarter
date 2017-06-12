2aba503b3545f869f0e9e39a8d9dafb8e993502f
maxavj <maxav.job@gmail.com>
2017-06-12 16:24:10 +0300

    Add Omega theme, Omega subtheme and regions

---
+++ themes/omega_sub/style/gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    rename = require('gulp-rename');
+
+gulp.task('styles', function() {
+  return gulp.src('scss/*.scss')
+    .pipe(sass({ style: 'expanded' }))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+    .pipe(gulp.dest('css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('css'));
+});
