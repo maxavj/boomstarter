f3d342c8f50fcd7065fc3cb6020997b5409d78b7
maxavj <maxav.job@gmail.com>
2017-06-23 14:47:02 +0300

    Add main page 1

--- themes/drupalbook/js/custom.js
+++ themes/drupalbook/js/custom.js
@@ -2,6 +2,14 @@ document.getElementsByName('keys')[0].placeholder='Название проект
 (function ($) {
 
       $( "#buttonvid" ).on( "click", function() {
-        $(".videocon").css("filter", "none");
+        $(".videocon").css("filter", "none").css("z-index", "999").css("width", "60%").get(0).play();
+          $(".podvideo, #buttonvid, .first-region p:nth-child(1), .first-region p:nth-child(2)").css("visibility", "hidden");
+          $(".first-region p:nth-child(3)").addClass('clicked');
+
 });
+      $("#butt_share").on("click", function() {
+        $(".fifteen-region .views-field-field-social").css("display", "block");
+      });
+      $("a[href='#']").removeAttr("href");
+
 })(jQuery);
