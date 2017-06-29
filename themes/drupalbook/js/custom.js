e50811975122c2c89a5f2768e97f138ee59b4767
maxavj <maxav.job@gmail.com>
2017-06-29 11:39:32 +0300

    Add /start

--- themes/drupalbook/js/custom.js
+++ themes/drupalbook/js/custom.js
@@ -7,12 +7,29 @@ document.getElementsByName('keys')[0].placeholder='Название проект
           $(".first-region p:nth-child(3)").addClass('clicked');
 
 });
+
+            $( ".path-start #buttonvid" ).on( "click", function() {
+        $(".videocon").css("display", "block").css("z-index", "9").css("width", "60%").get(0).play();
+          $(".podvideo, #buttonvid, .first-region p:nth-child(1), .first-region p:nth-child(2)").css("visibility", "hidden");
+          $(".first-region p:nth-child(3)").addClass('clicked');
+          $(".video-region .field--name-body p:nth-child(2)").css("background", "#fff");
+
+});
       $("#butt_share").on("click", function() {
         $(".fifteen-region .views-field-field-social, .seventeen-region .views-field-field-social").css("display", "block");
       });
       $("a[href='#']").removeAttr("href");
 
+      $(".left-region h5, .right-region h5").on("click", function() {
+        if ($(this).hasClass("q-clicked")) {
+          $(this).next().css("display", "none");
+          $(this).removeClass("q-clicked");
+        } else {
+          $(this).next().css("display", "block");
+          $(this).addClass("q-clicked");
+        };
 
+      });
       
 
 })(jQuery);
